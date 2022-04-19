4353cf7b2591fd7a0d7e315c786d00fd2f4ca76f
Rodrigo Araujo Barbosa <rodrigoarabarbosa@outlook.com>
2022-04-19 08:08:17 -0300

    testing coverlet + sonarcloud 1

--- ApiTemplate.WebApi-bkp/Jwt/Interfaces/ISigningConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.IdentityModel.Tokens;
-
-namespace ApiTemplate.WebApi.Jwt.Interfaces;
-
-public interface ISigningConfiguration
-{
-    SecurityKey Key { get; }
-    SigningCredentials SigningCredentials { get; }
-}
